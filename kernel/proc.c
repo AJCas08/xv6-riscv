eb0fce2220b909fbe6a8696bd5bd37ef96160890
ajcas08 <ajaydenc08@gmail.com>
2026-09-23 14:29:51 -0600

    Bug Fixes

--- kernel/proc.c
+++ kernel/proc.c
@@ -428,7 +428,6 @@ kwait2(uint64 addr, uint64 rusage_addr)
   struct proc *p = myproc();
 
   acquire(&wait_lock);
-
   for(;;){
     havekids = 0;
     for(pp = proc; pp < &proc[NPROC]; pp++){
@@ -437,9 +436,7 @@ kwait2(uint64 addr, uint64 rusage_addr)
         havekids = 1;
         if(pp->state == ZOMBIE){
           pid = pp->pid;
-
-          if(addr != 0 && copyout(p->pagetable, addr, (char *)&pp->xstate,
-                                  sizeof(pp->xstate)) < 0) {
+          if(addr != 0 && copyout(p->pagetable, p->sz, addr, (char *)&pp->xstate,sizeof(pp->xstate)) < 0){
             release(&pp->lock);
             release(&wait_lock);
             return -1;
@@ -448,8 +445,7 @@ kwait2(uint64 addr, uint64 rusage_addr)
           if(rusage_addr != 0){
             struct rusage ru;
             ru.cputime = pp->cputime;
-            if(copyout(p->pagetable, rusage_addr, (char *)&ru,
-                       sizeof(ru)) < 0) {
+            if(copyout(p->pagetable, p->sz, rusage_addr, (char *)&ru, sizeof(ru)) < 0){
               release(&pp->lock);
               release(&wait_lock);
               return -1;
