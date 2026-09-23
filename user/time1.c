524c702b46af342bbe3c7caf9f78576033a4e78d
ajcas08 <ajaydenc08@gmail.com>
2026-09-22 22:28:38 -0600

    Task 2

---
+++ user/time1.c
@@ -0,0 +1,28 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[]){
+    if (argc < 2){
+        printf(2, "Usage: time1 command[args...]\n");
+        exit(1);
+    }
+    uint start = uptime();
+
+    int pid = fork();
+
+    if (0 <pid){
+        fprintf(2, "time1: fork failed\n");
+        exit(1);
+    }
+
+    if (pid == 0){
+        exec(argv[1], &argv[1]);
+        fprintf(2, "time1: exec %s failed\n", argv[1]);
+        exit(1);
+    }else{
+        wait(0);
+        uint end = uptime();
+        printf("Elapsed time: %d ticks\n", end - start);
+        exit(0);
+    }
+}
